46a8e85a5c12c56a2038855c3e606f72da15a67f
Аркадий Алфёров <arhe414@gmail.com>
2025-09-18 17:40:58 +0300

    add new submodules

--- Gizmo.Client.UI.SteamPayment.Module/Components/SteamPaymentModule.razor.cs
+++ Gizmo.Client.UI.SteamPayment.Module/Components/SteamPaymentModule.razor.cs
@@ -1,7 +1,28 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Gizmo.Web.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace Gizmo.Client.UI.SteamPayment.Module.Components;
 
-public partial class SteamPaymentModule : ComponentBase
+public partial class SteamPaymentModule : CustomDOMComponentBase
 {
+    [Inject]
+    public UserMenuViewState UserMenuViewState { get; set; }
+
+    [Parameter]
+    public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+    protected override void OnInitialized()
+    {
+        this.SubscribeChange(UserMenuViewState);
+
+        base.OnInitialized();
+    }
+
+    public override void Dispose()
+    {
+        this.UnsubscribeChange(UserMenuViewState);
+
+        base.Dispose();
+    }
 }
